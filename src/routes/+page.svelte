d428de60a612c1ba07f5d4bececf2b35d41957bd
Sahil Kumar <sahilkr_96@outlook.com>
2024-03-29 17:06:44 +1100

    setup supabase ssr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+  export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{#if data.session}
+  <p>You are logged in as {data.session.user.email}</p>
+{:else}
+  <p>You are not logged in!</p>
+{/if}
