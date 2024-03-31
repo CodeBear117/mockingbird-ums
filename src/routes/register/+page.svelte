3035b14b88ea0432136b1e3035cb16e90919be08
Sahil Kumar <sahilkr_96@outlook.com>
2024-03-31 11:25:08 +1100

    basic styling for registration page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -49,32 +49,66 @@
 </script>
 
 <main>
-  <h1>Registration Page</h1>
-  <form method="POST" use:enhance={handleSubmit}>
-    <label>
-      <span>name</span>
-      <input type="name" name="name" placeholder="Your Name" required />
-    </label>
-    <label>
-      <span>email</span>
-      <input type="email" name="email" placeholder="Your Email" required />
-    </label>
-    <label>
-      <span>password</span>
-      <input type="password" name="password" placeholder="Password" required />
-    </label>
-    <button>Submit</button>
-    {#if state instanceof Error}
-      <div>
-        {state.message}
+  <div class="min-h-screen bg-slate-900 flex flex-col justify-center">
+    <div class="max-w-md w-full mx-auto">
+      <div class="bg-white p-8 border border-gray-300 rounded-lg">
+        <h1
+          class="font-bold text-3xl bg-gradient-to-r from-green-400 to-cyan-400 text-transparent bg-clip-text mb-3"
+        >
+          Register
+        </h1>
+        <p class="mb-4">
+          Already a user? Login <a href="/login" class="underline">here</a>
+        </p>
+        <form
+          method="POST"
+          use:enhance={handleSubmit}
+          class="flex flex-col justify-center"
+        >
+          <label>
+            <input
+              class="py-1 border rounded-lg text-center w-full mb-3"
+              type="name"
+              name="name"
+              placeholder="Your Name"
+              required
+            />
+          </label>
+          <label>
+            <input
+              class="py-1 border rounded-lg text-center w-full mb-3"
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+          </label>
+          <label>
+            <input
+              class="py-1 border rounded-lg text-center w-full mb-3"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </label>
+          <button
+            class="bg-gradient-to-r from-green-400 to-cyan-400 py-2 border rounded-lg hover:from-cyan-400 hover:to-green-400 text-white"
+            >Submit</button
+          >
+          {#if state instanceof Error}
+            <div>
+              {state.message}
+            </div>
+          {:else if typeof state === "object" && state.email}
+            <div>
+              We sent an email to {state.email} - Check your Inbox!
+            </div>
+          {:else if state === "loading"}
+            <Spinner size="8" />
+          {/if}
+        </form>
       </div>
-    {:else if typeof state === "object" && state.email}
-      <div>
-        We sent an email to {state.email} - Check your Inbox!
-      </div>
-    {:else if state === "loading"}
-      <Spinner size="8" />
-    {/if}
-  </form>
-  <p>Already a user? Login <a href="/login" class="underline">here</a></p>
+    </div>
+  </div>
 </main>
